17a3c0c21ef94415ab64e6d47b32e09c360daa88
Igone van_der_Kley <igone@vdkley.nl>
2023-12-15 15:56:41 +0100

    Allergeen data & product data query written.

--- app/models/CandyModel.php
+++ app/models/CandyModel.php
@@ -21,7 +21,7 @@ class CandyModel
     }
 
     function getleveringsInformatie($Id) {
-        $sql = "SELECT pro.Naam, prolev.DatumLevering, prolev.Aantal, prolev.DatumEerstVolgendeLevering, lev.Naam as Levnaam, lev.Contactpersoon, lev.LeverancierNummer, lev.Mobiel
+        $sql = "SELECT pro.Naam, pro.Barcode, prolev.DatumLevering, prolev.Aantal, prolev.DatumEerstVolgendeLevering, lev.Naam as Levnaam, lev.Contactpersoon, lev.LeverancierNummer, lev.Mobiel
                                                         FROM product pro
                                                         INNER JOIN ProductLeverancier prolev
                                                         ON pro.Id = prolev.ProductId
@@ -33,6 +33,27 @@ class CandyModel
                 ORDER BY prolev.DatumLevering asc";
 
         // $this->db->bind(':id', $Id);    
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getOverzichtAllergenen($Id) {
+        $sql = "SELECT alg.Naam, alg.Omschrijving
+                            FROM Allergeen alg
+                            INNER JOIN ProductPerAllergeen proper
+                            ON alg.Id = proper.AllergeenId
+                            INNER JOIN product pro
+                            ON proper.productId = pro.Id
+
+                WHERE pro.Id = $Id";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    function getProductData($Id) {
+        $sql = "SELECT Naam, Barcode FROM Product WHERE Id = $Id";
+
         $this->db->query($sql);
         return $this->db->resultSet();
     }
