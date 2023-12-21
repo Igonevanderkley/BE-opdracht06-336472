1529b9aeda21317df93b3a988f4b8a08b9616490
Igone van_der_Kley <igone@vdkley.nl>
2023-12-22 00:07:51 +0100

    GetContactInfo method added to controller.

--- app/controllers/candyShop.php
+++ app/controllers/candyShop.php
@@ -1,42 +1,44 @@
 <?php
 class CandyShop extends BaseController
 {
-    private $candyModel;
+    private CandyModel $candyModel;
 
     public function __construct()
     {
         $this->candyModel = $this->model('CandyModel');
     }
 
-    public function overzichtMagazijn() {
-
+    public function overzichtMagazijn()
+    {
         $magazijnData = $this->candyModel->getMagazijnOverzicht();
 
         $data = ['magazijnData' => $magazijnData];
-
         $this->view('CandyShop/overzichtMagazijn', $data);
-      
     }
 
-    public function leveringsInformatie($Id) {
+    public function leveringsInformatie($Id)
+    {
         $leveringsData = $this->candyModel->getLeveringsInformatie($Id);
+        $contactInfo = $this->candyModel->getContactInfo($Id);
 
-        $data = ['leveringsData' => $leveringsData];
 
+        if (!$leveringsData[0]->aantalAanwezig) {
+            header("Refresh: 4; url=/CandyShop/overzichtMagazijn");
+        }
+
+        $data = [
+            'leveringsData' => $leveringsData,
+            'contactInfo' => $contactInfo
+        ];
         $this->view('CandyShop/leveringsInformatie', $data);
     }
 
-    public function overzichtAllergenen($Id) {
+    public function overzichtAllergenen($Id)
+    {
         $allergeenData = $this->candyModel->getOverzichtAllergenen($Id);
-
-
         $productData = $this->candyModel->getProductData($Id);
 
         $data = ['allergeenData' => $allergeenData, 'productData' => $productData,];
-
         $this->view('CandyShop/overzichtAllergenen', $data);
     }
-
-
-  
 }
