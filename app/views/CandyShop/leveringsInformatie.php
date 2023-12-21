2cc5f787f857eb465c47789a131bc6e2ffbb9a6b
Igone van_der_Kley <igone@vdkley.nl>
2023-12-22 00:09:55 +0100

    Cleaned up view with alternative syntax.

--- app/views/CandyShop/leveringsInformatie.php
+++ app/views/CandyShop/leveringsInformatie.php
@@ -1,59 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= URLROOT; ?>/css/instructeurs.css">
     <title>leverings informatie</title>
 </head>
+
 <body>
     <h1>Leverings Informatie</h1>
-    <?php 
-    $leveringsData = $data['leveringsData'];
+    <?php foreach ($data["leveringsData"] as $levering) : $aantalAanwezig = $levering->aantalAanwezig; ?>
+        <?php foreach ($data['contactInfo'] as $contactInfo) ?> <?php endforeach ?>
+    <?php if ($aantalAanwezig > 0) : ?>
+        <ul>
+            <li>Leverancier: <?= $contactInfo->Naam ?></li>
+            <li>Contactpersoon: <?= $contactInfo->ContactPersoon ?></li>
+            <li>Leveranciernummer: <?= $contactInfo->LeverancierNummer ?></li>
+            <li>Mobiel: <?= $contactInfo->Mobiel ?></li>
+        </ul>
+    <?php endif ?>
 
-    $leveringTable = '';
+    <div class="tableDiv">
+        <table>
+            <thead>
+                <tr>
+                    <th>Naam Product</th>
+                    <th>Datum Laatse Levering</th>
+                    <th>Aantal</th>
+                    <th>EerstvolgendeLevering</th>
+                </tr>
+            </thead>
+            <?php foreach ($data["leveringsData"] as $levering) :
+                $aantalAanwezig = $levering->aantalAanwezig;
+            ?>
+                <?php if ($aantalAanwezig > 0) : ?>
+                    <tbody>
+                        <tr>
+                            <td> <?= $levering->Naam ?> </td>
+                            <td> <?= $levering->DatumLevering ?> </td>
+                            <td> <?= $levering->Aantal ?> </td>
+                            <td> <?= $levering->DatumEerstVolgendeLevering ?> </td>
+                        </tr>
+                    <?php else : ?>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>Er is van dit product op dit moment geen vooraad aanwezig, de verwachte eerstvolgende levering is: <?= $levering->DatumEerstVolgendeLevering ?></td>
+                        </tr>
+                    </tbody>
+                <?php endif ?>
+            <?php endforeach ?>
+        </table>
+    </div>
 
-    foreach ($leveringsData as $levering) {
-        $leveringTable .= "<tr>
-                            <td>$levering->Naam</td>
-                            <td>$levering->DatumLevering</td>
-                            <td>$levering->Aantal</td>
-                            <td>$levering->DatumEerstVolgendeLevering</td>
-                            </tr>";
-
-        $leverancier = $levering->Levnaam;
-        $contactpersoon = $levering->Contactpersoon;
-        $leverancierNummer = $levering->LeverancierNummer;
-        $mobiel = $levering->Mobiel;
-
-    }
-    ?>
-<ul>
-    <li>Leverancier: <?= $leverancier ?></li>
-    <li>Contactpersoon: <?= $contactpersoon ?></li>
-    <li>LeverancierNummer: <?= $leverancierNummer ?></li>
-    <li>Mobiel: <?= $mobiel ?></li>
-</ul>
-
-<div class="tableDiv">
-<table>
-    <thead>
-        <tr>
-            <th>Naam Product</th>
-            <th>Datum Laatse Levering</th>
-            <th>Aantal</th>
-            <th>EerstvolgendeLevering</th>
-        </tr>
-    </thead>
-
-    <tbody>
-        <?= $leveringTable ?>
-    </tbody>
-</table>
-</div>
 </body>
+
 </html>
-
-
-
