f0b0f4b51d44d92ec22b588d9438f321de8b0282
Igone van_der_Kley <igone@vdkley.nl>
2023-12-22 00:09:04 +0100

    GetContactInfo query added to Model.

--- app/models/CandyModel.php
+++ app/models/CandyModel.php
@@ -10,53 +10,65 @@ class CandyModel
     }
 
 
-    function getMagazijnOverzicht() {
-        $sql =  "SELECT pro.Id, pro.Barcode, pro.Naam, mag.VerpakkingsEenheid, mag.AantalAanwezig
-                                    FROM product pro
-                                    JOIN Magazijn mag
-                                    ON pro.Id = mag.ProductId
-                                    ORDER BY pro.Barcode asc";
+    function getMagazijnOverzicht()
+    {
+        $sql = "SELECT pro.Id, pro.Barcode, pro.Naam, mag.VerpakkingsEenheid, mag.AantalAanwezig
+                FROM product pro
+                JOIN Magazijn mag
+                ON pro.Id = mag.ProductId
+                ORDER BY pro.Barcode asc";
+
         $this->db->query($sql);
         return $this->db->resultSet();
     }
 
-    function getleveringsInformatie($Id) {
-        $sql = "SELECT pro.Naam, pro.Barcode, prolev.DatumLevering, prolev.Aantal, prolev.DatumEerstVolgendeLevering, lev.Naam as Levnaam, lev.Contactpersoon, lev.LeverancierNummer, lev.Mobiel
-                                                        FROM product pro
-                                                        INNER JOIN ProductLeverancier prolev
-                                                        ON pro.Id = prolev.ProductId
-                                                        INNER JOIN Magazijn mag
-                                                        ON pro.Id = mag.ProductId
-                                                        INNER JOIN leverancier lev
-                                                        ON lev.Id = prolev.LeverancierId
+    function getContactInfo($Id)
+    {
+        $sql = "SELECT lev.Naam, lev.ContactPersoon, lev.LeverancierNummer, lev.Mobiel
+                FROM Leverancier lev
+                INNER JOIN productLeverancier prolev
+                ON prolev.LeverancierId = lev.Id
+                WHERE prolev.productId = $Id";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    function getleveringsInformatie($Id)
+    {
+        $sql = "SELECT mag.aantalAanwezig, pro.Naam, pro.Barcode, prolev.DatumLevering, prolev.Aantal, prolev.DatumEerstVolgendeLevering
+                FROM product pro
+                INNER JOIN ProductLeverancier prolev
+                ON pro.Id = prolev.ProductId
+                INNER JOIN Magazijn mag
+                ON pro.Id = mag.ProductId
                 WHERE pro.Id = $Id
-                ORDER BY prolev.DatumLevering asc";
+                ORDER BY prolev.DatumLevering asc
+                ";
 
-        // $this->db->bind(':id', $Id);    
         $this->db->query($sql);
         return $this->db->resultSet();
     }
 
-    public function getOverzichtAllergenen($Id) {
+    public function getOverzichtAllergenen($Id)
+    {
         $sql = "SELECT alg.Naam, alg.Omschrijving
-                            FROM Allergeen alg
-                            INNER JOIN ProductPerAllergeen proper
-                            ON alg.Id = proper.AllergeenId
-                            INNER JOIN product pro
-                            ON proper.productId = pro.Id
-
+                FROM Allergeen alg
+                INNER JOIN ProductPerAllergeen proper
+                ON alg.Id = proper.AllergeenId
+                INNER JOIN product pro
+                ON proper.productId = pro.Id
                 WHERE pro.Id = $Id";
 
         $this->db->query($sql);
         return $this->db->resultSet();
     }
 
-    function getProductData($Id) {
+    function getProductData($Id)
+    {
         $sql = "SELECT Naam, Barcode FROM Product WHERE Id = $Id";
 
         $this->db->query($sql);
         return $this->db->resultSet();
     }
-
-
 }
